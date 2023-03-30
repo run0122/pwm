e64cdffe92c70a5414fbec1d71a00e05891dd5b5
run0122 <run0122@naver.com>
2023-03-30 15:23:56 +0900

    10단계 밝기 조절

--- pwm.ino
+++ pwm.ino
@@ -4,13 +4,16 @@ const int LED = 10;
 
 void setup() {
   Timer1.initialize();
-  Timer1.pwm(LED,0);
+  Timer1.pwm(LED, 0);
 
-  Timer1.setPeriod(1000000/10);
-  Timer1.setPwmDuty(LED, 100);
+  // Timer1.setPeriod(1000000/10);
+  // Timer1.setPwmDuty(LED, 100);
+  Timer1.setPeriod(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (int t_high = 0; t_high <= 10; t_high++) {
+    Timer1.setPwmDuty(LED, t_high * 100);
+    delay(100);
+  }
 }
